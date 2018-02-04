d9f8ce253ec24b7fd754bf1b140d910f57c537c3
Ravi Malik <ravimalik2364@gmail.com>
2018-02-04 14:58:24 -0500

    Basics: Variables

---
+++ 1_variables.R
@@ -0,0 +1,24 @@
+a <- 4L
+b <- 2.5
+typeof(a)
+typeof(b)
+
+c <- "h"
+q <- T
+
+a <- a + b
+a
+
+div = a / b
+mul = a * b
+
+div
+mul
+
+s <- sqrt(a)
+s
+
+greeting <- "Hello"
+name <- "Ravi"
+message <- paste(greeting, name)
+message
