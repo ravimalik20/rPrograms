2047e9af0dc4c5a838766e1ba302837c42f55b78
Ravi Malik <ravimalik2364@gmail.com>
2018-02-04 15:40:17 -0500

    Basics: loops

---
+++ 3_loops.R
@@ -0,0 +1,12 @@
+i <- 0
+while (i < 10) {
+  print (i)
+  i <- i + 1
+}
+
+# includive of 1 to 10. Basically iterating over a vector. 1:10 generates a vector
+for (i in 1:10) {
+  print (i)
+}
+
+print (1:10)
